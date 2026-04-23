d8ed4229b27cff702c7dba8c5302d75c0078db69
ancatudorache <anca.tudorache05@gmail.com>
2026-04-24 01:17:17 +0300

    LoginActivity+DriverDashboard logic

--- app/src/main/java/com/example/logisticsoptimization/DriverDashboardActivity.java
+++ app/src/main/java/com/example/logisticsoptimization/DriverDashboardActivity.java
@@ -3,20 +3,39 @@ package com.example.logisticsoptimization;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import org.json.JSONArray;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.example.logisticsoptimization.Delivery;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DriverDashboardActivity extends AppCompatActivity {
     TextView tvWelcome;
-
+    RecyclerView recyclerViewDeliveries;
+    List<Delivery> deliveryList = new ArrayList<>();
+    DeliveryAdapter adapter;
+    Delivery selectedDelivery = null;
+    MaterialButton btnStartDelivery;
+    int userId;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,15 +43,79 @@ public class DriverDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_driver_dashboard);
+
         tvWelcome = findViewById(R.id.tvWelcome);
+        btnStartDelivery = findViewById(R.id.btnStartDelivery);
+        
         String username = getIntent().getStringExtra("username");
+        userId = getIntent().getIntExtra("userId", -1);
+
+        if(username != null){
+            tvWelcome.setText("Welcome, " + username + "!");
+        }
+        
+        recyclerViewDeliveries = findViewById(R.id.recyclerViewDeliveries);
+        adapter = new DeliveryAdapter(this, deliveryList, delivery -> {
+            this.selectedDelivery = delivery;
+        });
+        
+        recyclerViewDeliveries.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewDeliveries.setAdapter(adapter);
+        
+        loadDeliveries();
+
+        btnStartDelivery.setOnClickListener(v -> {
+            if (selectedDelivery != null) {
+                Intent intent = new Intent(DriverDashboardActivity.this, ActiveDeliveryActivity.class);
+                intent.putExtra("deliveryId", selectedDelivery.getId());
+                intent.putExtra("pickupAddress", selectedDelivery.getPickupAddress());
+                intent.putExtra("deliveryAddress", selectedDelivery.getDeliveryAddress());
+                intent.putExtra("deadline", selectedDelivery.getDeadline());
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Please select a delivery first", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        if(username != null){
-            tvWelcome.setText("Welcome, " + username + "!");
-        }
+    }
+
+    private void loadDeliveries() {
+        String url = "http://192.168.0.193:3000/api/deliveries/driver/" + userId;
+
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    try {
+                        deliveryList.clear();
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject obj = response.getJSONObject(i);
+                            Delivery delivery = new Delivery(
+                                    obj.getInt("id"),
+                                    obj.getString("pickup_address"),
+                                    obj.getString("delivery_address"),
+                                    obj.getString("deadline")
+                            );
+                            deliveryList.add(delivery);
+                        }
+                        adapter.notifyDataSetChanged();
+
+                    } catch ( JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    Log.e("DELIVERIES", "Eroare: " + error.toString());
+                    Toast.makeText(this, "Eroare la incarcarea livrarilor", Toast.LENGTH_SHORT).show();
+                }
+        ) {};
+
+        Volley.newRequestQueue(this).add(request);
     }
 }
--- app/src/main/java/com/example/logisticsoptimization/MainActivity.java
+++ app/src/main/java/com/example/logisticsoptimization/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.logisticsoptimization;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONObject;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextInputEditText usernameInputText;
@@ -41,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loginUser() {
-        String username = usernameInputText.getText().toString();
-        String password = passwordInputText.getText().toString();
+        String username = usernameInputText.getText().toString().trim();
+        String password = passwordInputText.getText().toString().trim();
 
         if(username.isEmpty()){
             usernameInputText.setError("Username cannot be empty");
@@ -55,24 +64,54 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // Fake login by role
-        if (username.equals("dispatcher1") && password.equals("1234")) {
-            Intent intent = new Intent(MainActivity.this, DispatcherDashboardActivity.class);
-            intent.putExtra("username",username);
-            startActivity(intent);
-            finish();
-        } else if (username.equals("manager1") && password.equals("1234")) {
-            Intent intent = new Intent(MainActivity.this, ManagerDashboardActivity.class);
-            intent.putExtra("username",username);
-            startActivity(intent);
-            finish();
-        } else if (username.equals("driver1") && password.equals("1234")) {
-            Intent intent = new Intent(MainActivity.this, DriverDashboardActivity.class);
-            intent.putExtra("username",username);
-            startActivity(intent);
-            finish();
-        } else {
-            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+        JSONObject jsonBody = new JSONObject();
+        try{
+            jsonBody.put("username", username);
+            jsonBody.put("password", password);
+        }catch (Exception e){
+            e.printStackTrace();
+            return;
         }
+        String url = "http://192.168.0.193:3000/api/login";
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonBody,
+                response -> {
+
+                    try {
+                        String role = response.getJSONObject("user").getString("role");
+                        String name = response.getJSONObject("user").getString("name");
+                        int userId = response.getJSONObject("user").getInt("id");
+
+
+                        if (role.equals("manager")) {
+                            Intent intent = new Intent(MainActivity.this, ManagerDashboardActivity.class);
+                            intent.putExtra("username", name);
+                            startActivity(intent);
+                            finish();
+                        } else if (role.equals("dispatcher")) {
+                            Intent intent = new Intent(MainActivity.this, DispatcherDashboardActivity.class);
+                            intent.putExtra("username", name);
+                            startActivity(intent);
+                            finish();
+                        } else if (role.equals("driver")) {
+                            Intent intent = new Intent(MainActivity.this, DriverDashboardActivity.class);
+                            intent.putExtra("username", name);
+                            intent.putExtra("userId", userId);
+                            startActivity(intent);
+                            finish();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    Toast.makeText(MainActivity.this, "Username sau parola incorecte", Toast.LENGTH_SHORT).show();
+
+                }
+        ) {};
+        Volley.newRequestQueue(this).add(request);
     }
 }
